eed5abd0ea67d7d54e5897c627dedc2f06627564
94-c <hyeongwoo26@gmail.com>
2023-07-20 16:52:06 +0900

    # 도메인 생성

---
+++ src/main/java/com/spring/blog/model/Event.java
@@ -0,0 +1,27 @@
+package com.spring.blog.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant date;
+    private String title;
+    private String description;
+    @ManyToMany
+    private Set<User> attendees = new HashSet<>();
+}
---
+++ src/main/java/com/spring/blog/model/User.java
@@ -0,0 +1,22 @@
+package com.spring.blog.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "user_id")
+    private String userId;
+    private String name;
+    private String email;
+
+}

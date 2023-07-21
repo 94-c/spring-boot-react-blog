c7abe3906e6aa50f25ad7df30d1b839573476c9c
94-c <hyeongwoo26@gmail.com>
2023-07-21 18:33:08 +0900

    # 도메인 수정 및 pom.xml 추가

--- src/main/java/com/spring/blog/model/User.java
+++ src/main/java/com/spring/blog/model/User.java
@@ -5,16 +5,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String userId;
+    private String id;
     private String name;
     private String email;
-
 }

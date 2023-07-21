87373cab1db02efbad87d0d1f03d0803f88ee383
94-c <hyeongwoo26@gmail.com>
2023-07-21 18:20:21 +0900

    # Csrf 토큰으로 로그인 구현 Backend 개발 완료

--- src/main/java/com/spring/blog/repository/GroupRepository.java
+++ src/main/java/com/spring/blog/repository/GroupRepository.java
@@ -4,8 +4,10 @@ import com.spring.blog.model.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
     Group findByName(String name);
-
+    List<Group> findAllByUserId(String id);
 }
